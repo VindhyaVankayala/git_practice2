317df14c300f7b706828330a00521f36af728b80
Vindhya <vindhya24@outlook.com>
2024-03-29 13:29:22 +0530

    "Second commit"

---
+++ src/main/java/com/example/git_practice2/GitPractice2Application.java
@@ -0,0 +1,13 @@
+package com.example.git_practice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPractice2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitPractice2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/git_practice2/GitPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_practice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPractice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
